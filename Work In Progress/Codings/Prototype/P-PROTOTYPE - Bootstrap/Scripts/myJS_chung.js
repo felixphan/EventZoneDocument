4f7eba75d012b161d77cad4d98d8f4e8edca3c1e
duongnnse02205 <duongnnse02205@gmail.com>
2015-10-01 04:27:58 +0700

    Chính - Index.html
    Bỏ Captcha
    Thêm plugin mới: uikit.css
    Nâng cấp Category
    Chuyển Help, Policy vào Brand
    Chỉnh Video fit màn hình
    Chỉnh Video chạy lặp
    
    Phụ - Search_result.html
    
    Lấy làm chuẩn Top menu mới

--- Work In Progress/Codings/Prototype/P-PROTOTYPE - Bootstrap/Scripts/myJS_chung.js
+++ Work In Progress/Codings/Prototype/P-PROTOTYPE - Bootstrap/Scripts/myJS_chung.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 	
-	/*** xử lý khi Load trang ***/
+/*** xử lý khi Load trang ***/
 	var windows_height = $(window).height();	// Lấy chiều cao Browser
 	var windows_width = $(window).width();	// Lấy chiều ngang Browser
 	$('.d_bottom_3_column_container').css("top", windows_height);	// che 3 cột bằng chiều cao Browser
 	$('.d_bottom_3_column_container').transition({ y: '-50px' });	// cho 3 columns nhô lên
-	$('#i_bg_video_blur').transition({ opacity: 0 });		// Blur nền khi hiện 3 cột
+	$('#i_bg_video_blur').transition({ opacity: 0 }, 200);		// Blur nền khi hiện 3 cột
 	$('#i_btn_Cate').css("top", "-20px");	// Hide Category đi	//
 	$('.text1_container').transition({scale: 0}, 0); // Đưa ra ngoài để trôi vào //
 	$('.text2_container').transition({scale: 0}, 0);
@@ -14,7 +14,7 @@ $(document).ready(function(){
 	$('.text2_container').transition({ scale: 1 }, 1000, 'ease');
 	$('.cate_container').transition({ scale: 1 }, 1800, 'ease');
 
-	/*** Xử lý tích chọn All Category thì tất cả Category khác đều được tích ***/
+/*** Xử lý tích chọn All Category thì tất cả Category khác đều được tích ***/
 	var item_All_is_checked = false;
 	$('#i_chbox_All').click(function(){
 		if($('#i_chbox_All').prop('checked')){
@@ -25,7 +25,26 @@ $(document).ready(function(){
 	});
 	/*** End of Xử lý tích chọn All Category thì tất cả Category khác đều được tích ***/
     
-    /*** Scroll 3 columns sự kiện lên/xuống ***/
+/*** Nâng cấp tắt, mở Category ***/
+	$('#i_btn_Cate').click(function(){
+		$('#i_Cate_dropdown').slideToggle();
+	});
+	/*** End of Nâng cấp tắt, mở Category ***/
+
+/*** Kích hoạt Sign In, Sign Up tương ứng
+	$('#i_btn_signin').click(function(){
+		$('.d_signup_tab').removeClass('active');
+		$('.d_signin_tab').addClass('active');
+		$('#i_signin_section').addClass('active');
+	});
+	$('#i_btn_signup').click(function(){
+		$('.d_signin_tab').removeClass('active');
+		$('.d_signup_tab').addClass('active');
+		$('#i_signup_section').addClass('active');
+	}); ***/
+	/*** End of Nâng cấp tắt, mở Category ***/
+
+/*** Scroll 3 columns sự kiện lên/xuống ***/
     var lastScrollTop = 0;
     $(window).scroll(function(event){
    	var st = $(this).scrollTop();
@@ -42,12 +61,13 @@ $(document).ready(function(){
 	        $('.d_middle_container').transition({ opacity: 1, delay: 100});
 	        $('#i_search').transition({x: "0px"}, 'ease');
 	        $('#i_btn_Cate').transition({y: "0px"}, 'ease');
+	        $('#i_Cate_dropdown').slideUp();
 	    }
 	    lastScrollTop = st;
 	});
 	/*** End of Scroll 3 columns sự kiện lên/xuống ***/
 
-    /*** OUTSOURCE: tùy chỉnh scroll bar ***/
+/*** OUTSOURCE: tùy chỉnh scroll bar ***/
 	function changeSize() {
     var width = parseInt($("#Width").val());
     var height = parseInt($("#Height").val());
@@ -69,14 +89,14 @@ $(document).ready(function(){
 	});
 	/*** End of OUTSOURCE: tùy chỉnh scroll bar ***/
 
-	/***  ***/
+/***  ***/
 	var logoHeight = $('#d_thumb img').height();
     if (logoHeight < 90) {
         var margintop = (90 - logoHeight) / 2;
         $('#d_thumb img').css('margin-top', margintop);
     }
 
-    /*** BOOTSTRAP: tooltip ***/
+/*** BOOTSTRAP: tooltip ***/
 	$(document).ready(function(){
 	    $('[data-toggle="tooltip"]').tooltip();   
 	});
---
+++ Work In Progress/Codings/Prototype/P-PROTOTYPE - Bootstrap/Scripts/myJS_s_r.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+/*** xử lý khi Load trang ***/
+
+
+/*** Nâng cấp tắt, mở Category ***/
+	$('#i_btn_Cate').click(function(){
+		$('#i_Cate_dropdown').slideToggle();
+	});
+	/*** End of Nâng cấp tắt, mở Category ***/
+});
