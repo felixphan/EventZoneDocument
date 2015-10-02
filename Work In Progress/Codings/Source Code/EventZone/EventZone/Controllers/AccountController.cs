ec4fd441494ec65bf32f72272902acb668c4151c
cuongnv764119 <cuongnv764119@gmail.com>
2015-10-03 00:54:18 +0700

    Update register + login (chưa gg)

---
+++ Work In Progress/Codings/Source Code/EventZone/EventZone/Controllers/AccountController.cs
@@ -0,0 +1,128 @@
+﻿using EventZone.Helpers;
+using EventZone.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EventZone.Controllers
+{
+    public class AccountController : Controller
+    {
+        EventZoneEntities db = new EventZoneEntities();
+        // GET: /Account/Register
+        [AllowAnonymous]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Account/Register
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User();
+                List<User> listUser = new List<User>();
+                listUser = db.Users.ToList();
+                var newUser = listUser.FindAll(a => a.UserEmail.Equals(model.UserName));
+
+                if (newUser.Count == 0)
+                {
+                    user.TypeID = 0;// eventzone account
+                    //Set all parameters for user from registerViewModel
+                    user.UserEmail = model.UserName;
+                    user.UserPassword = model.Password;
+                    user.UserDOB = model.UserDOB;
+                    user.UserFirstName = model.UserFirstName;
+                    if (model.UserLastName != null && model.UserLastName != "")
+                    {
+                        user.UserLastName = model.UserLastName;
+                    }
+                    user.AccountStatus = EventZoneConstants.IsUserActive;//set Active account
+                    if (string.IsNullOrWhiteSpace(user.AvatarLink))//set default avatar
+                    {
+                        user.AvatarLink = "/img/default-avatar.jpg";
+                    }
+                    user.UserRoles = EventZoneConstants.IsUser;//set UserRole
+                    // insert user to Database
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                    Session["registerMessageError"] = "";
+                }
+                else {
+                    ModelState.AddModelError("", "User with the email you provided is already exist.");
+                    return View();
+                }
+                //set all session for 
+                Session["authenticated"] = true;
+                Session["userName"] = user.UserFirstName;
+                Session["userAva"] = user.AvatarLink;
+                Session["UserId"] = user.UserID;
+                UserHelpers.SetCurrentUser(Session, user);
+
+                //Send email confirm
+                MailHelpers.Instance.SendMailWelcome(user.UserEmail, user.UserFirstName, user.UserLastName);
+            }
+
+            // If we got this far, something failed, redisplay form
+            return RedirectToAction("RegisterSuccess", "Account");
+        }
+        public ActionResult RegisterSuccess()
+        {
+            return View();
+        }
+
+        // GET: /Account/Login
+        [AllowAnonymous]
+        public ActionResult Signin(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        //
+        // POST: /Account/Login
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Signin(LoginViewModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                DatabaseHelpers dbhelp= new DatabaseHelpers();
+                if (dbhelp.ValidateUser(model.UserName, model.Password)) {
+                    if (dbhelp.isLookedUser(model.UserName))
+                    {
+                        ModelState.AddModelError("", "Your account is locked! Please contact with our support");
+                    }
+                    else {
+                        var user = dbhelp.GetUserByEmail(model.UserName);
+                        Session["authenticated"] = true;
+                        Session["userName"] = user.UserFirstName;
+                        Session["userAva"] = user.AvatarLink;
+                        Session["UserId"] = user.UserID;
+                        UserHelpers.SetCurrentUser(Session, user);
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Email or password is invalid.");
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(returnUrl);
+        }
+
+
+
+    }
+}
---
+++ Work In Progress/Codings/Source Code/EventZone/EventZone/Models/AccountViewModels.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EventZone.Models
+{
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+    }
+
+    public class ManageUserViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Current password")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "User name")]
+        [EmailAddress(ErrorMessage = "The email format is incorrect")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+    }
+
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Please enter your email")]
+        [EmailAddress(ErrorMessage = "The email format is incorrect")]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your password")]
+        [StringLength(100, ErrorMessage = "The {0} must more than {2} characters.", MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "ConfirmPassword")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Date of birth")]
+        public Nullable<System.DateTime> UserDOB { get; set; }
+
+
+        [Required(ErrorMessage="Please select gender")]
+        public int Gender { get; set; }
+
+        [Required(ErrorMessage="Please enter your first name!")]
+        [Display(Name="First name")]
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+    }
+}
