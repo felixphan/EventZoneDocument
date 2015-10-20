c8a0521e0c74ef3e02a2a95dda0397d6f4c0d8bc
chuongnt13 <chuongntse02649@fpt.edu.vn>
2015-10-20 19:25:17 +0700

    Admin function

---
+++ Work In Progress/Codings/Source Code/EventZone_ChuongNT/EventZoneC/EventZoneC/Helpers/UserHelper.cs
@@ -0,0 +1,40 @@
+﻿using EventZoneC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventZoneC.Helpers
+{
+    public class UserHelpers
+    {
+        private EventZoneEntities3 db = new EventZoneEntities3();
+
+
+        private const string User = "user";
+        public static User GetCurrentUser(HttpSessionStateBase session)
+        {
+
+            var userSession = session[User];
+
+            if (userSession == null || userSession.ToString().Length == 0)
+            {
+                return null;
+            }
+            var user = (User)userSession;
+
+            return user;
+        }
+        public static void SetCurrentUser(HttpSessionStateBase session, User user)
+        {
+            session[User] = user;
+        }
+        public  User getUser(string userName)
+        {
+            var user = db.Users.ToList();
+            user = user.FindAll(u => u.UserName.Contains(userName));
+            return (User)user[0];
+        }
+
+    }
+}
