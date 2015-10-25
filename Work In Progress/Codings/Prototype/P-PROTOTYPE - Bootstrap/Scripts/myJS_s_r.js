547fe1919962fb20c5b60f022b292752206d1e37
duongnnse02205 <duongnnse02205@gmail.com>
2015-10-25 16:34:25 +0700

    Tiếp: tách các phần thành iframe. index_2.html: Homepage mới, cơ bản xong (còn cần lắp top_menu.html) top_menu.html: cần tách iframe, thêm dòng search query. search_result.html: cơ bản xong (còn result hiện theo Location)

--- Work In Progress/Codings/Prototype/P-PROTOTYPE - Bootstrap/Scripts/myJS_s_r.js
+++ Work In Progress/Codings/Prototype/P-PROTOTYPE - Bootstrap/Scripts/myJS_s_r.js
@@ -1,11 +1,7 @@
 $(document).ready(function(){
 /*** xử lý khi Load trang ***/
-	
-	/* Cho Avatar thành hình vuông: chiều cao = chiều ngang */
-	$(window).resize(function(){
-		
-	);
 
-	$('.d_u_avatar').css("height", $('.d_u_avatar').width());
+
+
 
 });
