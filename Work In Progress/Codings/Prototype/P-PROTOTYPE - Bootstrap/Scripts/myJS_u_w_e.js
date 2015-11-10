9da4c361910d21ac613a6aca582cc57d6d79f60d
duongnnse02205 <duongnnse02205@gmail.com>
2015-11-10 21:47:27 +0700

    Update User_watch_event.html (Còn thay đổi nội dung các Lý do Report)

--- Work In Progress/Codings/Prototype/P-PROTOTYPE - Bootstrap/Scripts/myJS_u_w_e.js
+++ Work In Progress/Codings/Prototype/P-PROTOTYPE - Bootstrap/Scripts/myJS_u_w_e.js
@@ -1,29 +1,37 @@
 $(document).ready(function(){
 
-	/*** SCRIPT xử lý ngay khi Load trang ***/
-	if($('#i_event_thumbnail').width() < $('#i_event_thumbnail').height()){
-		$('.crop img').addClass('portrait');
-	}
+/******************************************** SCRIPT xử lý ngay khi Load trang ***/
+	$('.d_cmt_after').css("display", "none");	//Hide Comment
+	$('.d_reopen_map').css("display", "none"); //Hide nút Re-open Map
 
-	$('#i_comment_btn').click(function(){
-		$('.d_comment_report').animate({
-		    height: '500'
+/******************************************** Animation Mở Comment, Hide Map ***/
+	$('.d_txt_write_cmt').click(function(){
+		$('.d_right_bottom').animate({
+		    height: '550px'
 		}, {
-		    duration: 500,
+		    duration: 600,
 		    queue: false,   //queue: false giúp các Animation chạy đồng thời
 		    complete: function() { /* Animation complete */ }
 		});
+		$('.d_right_top').slideToggle( "slow" ); //Hide Map đi
+		$('.d_cmt_before').slideToggle( "slow" ); //Hide Sample comment
+		$('.d_cmt_after').slideToggle( "slow" ); //Display Comment
+		$('.d_reopen_map').slideToggle( "slow" ); //Display nút Re-open Map
+	});
 
-		$('.d_comment_report').animate({
-		    top: '-300'
+/******************************************** Animation Hide Comment, Mở Map ***/
+	$('.d_reopen_map').click(function(){
+		$('.d_right_bottom').animate({
+		    height: '230px'
 		}, {
-		    duration: 500,
-		    queue: false,
+		    duration: 600,
+		    queue: false,   //queue: false giúp các Animation chạy đồng thời
 		    complete: function() { /* Animation complete */ }
 		});
-		
-		$('.d_cmt_content_cover').addClass('d_cmt_extend');
-		$('.d_cmt_content_cover').addClass('d_cmt_extend_2');
+		$('.d_right_top').slideToggle( "slow" ); //Mở lại Map
+		$('.d_cmt_before').slideToggle( "slow" ); //Mở lại Sample comment
+		$('.d_cmt_after').slideToggle( "slow" ); //Hide Comment
+		$('.d_reopen_map').slideToggle( "slow" ); //Hide nút Re-open Map
 	});
 	
 	/*** OUTSOURCE: Google Map ***/
