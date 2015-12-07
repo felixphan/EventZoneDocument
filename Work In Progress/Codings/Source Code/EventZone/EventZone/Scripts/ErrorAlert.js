2d64f25a681e690d723157768a9ae547ca5ba0b5
felixphan <heaven13>
2015-12-07 21:00:28 +0700

    - Commit gheps Code duobng 7/12 - Xoa 2 folder code cu cho nhe ------------------------------- Ghep scrollbar chua hoan chinh (vo khung)

--- Work In Progress/Codings/Source Code/EventZone/EventZone/Scripts/ErrorAlert.js
+++
@@ -1,6 +0,0 @@
-﻿function errorShow(title, message) {
-    $("#error-title").text(title)
-    console.log(title)
-    $("#error-message").text(message)
-    $("#error-modal").modal('show')
-}
--- Work In Progress/Codings/Source Code/EventZone_Remake/EventZone/EventZone/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EventZone.MvcApplication" Language="C#" %>
